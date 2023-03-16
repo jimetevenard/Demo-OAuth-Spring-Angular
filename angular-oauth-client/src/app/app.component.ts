9f5d9da360fb2a5f2aec8edde4763ff038f261dc
Jim ETEVENARD <jetevenard@oxiane.com>
2023-03-16 17:35:54 +0100

    [Angular] Appels aux endpoints
    Et affichage du résultat dans le composant principal

--- angular-oauth-client/src/app/app.component.ts
+++ angular-oauth-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient) {}
+
   title = 'angular-oauth-client';
+  aboutMessage: string = '';
+  greetingMessage: string = '';
+
+  ngOnInit(): void {
+    this.http.get('/api/about', {responseType: 'text'}).subscribe((data: any) => {
+      this.aboutMessage = data;
+    });
+
+    this.http.get('/api/hello', {responseType: 'text'}) // On affiche le JSON brut.
+      .subscribe({
+        error: (e) => { this.greetingMessage = `ERREUR : ${e.status} ${e.statusText}`; },
+        next: (response) => { this.greetingMessage = response; }
+    });
+  }
 }
